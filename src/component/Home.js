90b76f8c4db89f84dcf8f23b4b749ea6ff0bd7b7
murugappansrija <srijamurugappan66@gmail.com>
2023-06-27 13:38:32 +0530

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+function Home() {
+  return (
+    <div>
+    <Box height={100} />
+    <div>
+    <img className='libImage'
+    src="https://images.unsplash.com/photo-1579370318443-8da816457e3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bGlicmFyaWVzfGVufDB8fDB8fHww&w=1000&q=80" alt='Library' />
+    </div>
+    </div>
+  )
+}
+
+export default Home
